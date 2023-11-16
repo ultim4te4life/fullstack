f000d5826ef226c767bbaa23b664edafbd00b7a8
Admin <admin@Admins-MacBook-Pro.local>
2023-11-15 20:57:11 -0800

    added env file order order to hide local port

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
-const port = 8000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("hello world!");
