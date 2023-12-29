faf3489917330e3e0e1d55feb1897e9e92aadfea
Admin <admin@Admins-MacBook-Pro.local>
2023-12-28 21:03:11 -0800

    added some homepage things

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -4,10 +4,14 @@ import { Header } from "../../components";
 
 export const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <Header />
 
-      <div>This is home page</div>
+      <div className="main-content">
+        <h1>Welcome to Your Website</h1>
+        <p>Explore the world of possibilities</p>
+        <button className="explore-button">Explore Now</button>
+      </div>
     </div>
   );
 };
