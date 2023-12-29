13ecdb2728ab0aa4907e4737c79b709e5a0406bc
Admin <admin@Admins-MacBook-Pro.local>
2023-12-28 18:43:41 -0800

    changed cors

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,17 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: [
+      "https://fullstack-frontend-omega.vercel.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
 require("dotenv").config();
 const usersRoutes = require("./routes/users");
 const productsRoutes = require("./routes/products");
